8aa90b83b5f42d8a1a25dd1df685483967d594ec
sara-munoz-5 <munozsara@javeriana.edu.co>
2025-10-02 18:25:04 -0500

    pruebas manuales

--- src/main/java/com/example/demo/model/Creature.java
+++ src/main/java/com/example/demo/model/Creature.java
@@ -1,20 +1,22 @@
-package com.example.demo.model; // #Paquete del proyecto
+package com.example.demo.model;
 
-import jakarta.persistence.Entity;     // JPA (para mapear a la base de datos)
-import jakarta.persistence.GeneratedValue;             // Lombok (genera getters/setters/toString)
-import jakarta.persistence.GenerationType; // Lombok (constructor vacío)
-import jakarta.persistence.Id; // Importa la entidad Zone
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity
-@Data //evita escribir getters y setters
-@NoArgsConstructor // Constructor vacío
+@Data
+@NoArgsConstructor
 public class Creature {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincremental
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
@@ -23,7 +25,6 @@ public class Creature {
     private int dangerLevel;
     private String healthStatus;
 
-   
     @ManyToOne
     @JoinColumn(name = "zone_id")
     private Zone zone;
